14c0a580205a2f8bd1a66078f15523462c04afbc
Markus Rothlübbers <markus@mrlb.space>
2023-10-10 18:47:41 +0200

    Add basic service layer

---
+++ HeartRateBandCalc.Wasm/Services/HeartService.cs
@@ -0,0 +1,6 @@
+namespace HeartRateBandCalc.Wasm.Services;
+
+public class HeartService : IHeartService
+{
+    
+}
---
+++ HeartRateBandCalc.Wasm/Services/IHeartService.cs
@@ -0,0 +1,6 @@
+namespace HeartRateBandCalc.Wasm.Services;
+
+public interface IHeartService
+{
+    
+}
