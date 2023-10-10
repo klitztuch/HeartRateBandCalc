e653ec3796c41e644e2d31743f901416304be786
Markus Rothlübbers <markus@mrlb.space>
2023-10-10 19:04:00 +0200

    Register HeartService

--- HeartRateBandCalc.Wasm/Program.cs
+++ HeartRateBandCalc.Wasm/Program.cs
@@ -1,13 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using HeartRateBandCalc.Wasm;
+using HeartRateBandCalc.Wasm.Services;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IHeartService, HeartService>();
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
